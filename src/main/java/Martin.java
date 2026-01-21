a86982e03b5f173092ffda95279f9e0015914664
joel ong <joelong345@gmail.com>
2026-01-21 11:15:33 +0800

    Rename Duke to Martin and Add Simple hello and bye

---
+++ src/main/java/Martin.java
@@ -0,0 +1,7 @@
+public class Martin {
+    public static void main(String[] args) {
+        System.out.println("hello sir my name is Martin\nWhat can I clean for you today?");
+        System.out.println("_".repeat(40));
+        System.out.println("Sorry Sir, Today is my off day. See you tomorrow!");
+    }
+}
