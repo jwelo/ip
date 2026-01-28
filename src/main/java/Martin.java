e73085ace91e6842655d86601cbb0422526a2cc2
joel ong <joelong345@gmail.com>
2026-01-28 11:21:24 +0800

    add echo functionality

--- src/main/java/Martin.java
+++ src/main/java/Martin.java
@@ -1,7 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Martin {
-    public static void main(String[] args) {
-        System.out.println("hello sir my name is Martin\nWhat can I clean for you today?");
+    private static ArrayList<String> listOfActivities = new ArrayList<>();
+
+    private static void storeActivity(String activity) {
+        listOfActivities.add(activity);
+    }
+
+    private static void printHorizontalLine() {
         System.out.println("_".repeat(40));
-        System.out.println("Sorry Sir, Today is my off day. See you tomorrow!");
+    }
+
+    public static void main(String[] args) {
+        printHorizontalLine();
+        System.out.println("Martin: \nHello sir my name is Martin.\nWhat can I do for you today?");
+        printHorizontalLine();
+        //System.out.println("Martin: Sorry Sir, Today is my off day. See you tomorrow!");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("User Command: ");
+        String line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            // echo
+            printHorizontalLine();
+            System.out.println("Martin:\n" + line);
+            printHorizontalLine();
+
+            System.out.println("User Command: ");
+            line = in.nextLine();
+        }
+        printHorizontalLine();
+        System.out.println("Martin:\nBye Sir, see you tomorrow!");
+        printHorizontalLine();
     }
 }
