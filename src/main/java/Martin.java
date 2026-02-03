cb85c2da7f1401c3aac2c59370a2e7621c7d300a
joel ong <joelong345@gmail.com>
2026-02-03 13:22:00 +0800

    Improve code quality by extracting methods

--- src/main/java/Martin.java
+++ src/main/java/Martin.java
@@ -3,10 +3,47 @@ package src.main.java;
 import java.util.Scanner;
 
 public class Martin {
-    //private static ArrayList<String> listOfActivities = new ArrayList<String>();
-    //private static ArrayList<Boolean> markedAsDone = new ArrayList<Boolean>();
+    public static final int HORIZONTAL_LINE = 40;
     private static Task[] tasks = new Task[100];
     private static int taskCounter = 0;
+
+    public static void main(String[] args) {
+        Scanner in = getFirstScanner();
+
+        label:
+        while (true) {
+            // Ask for next command
+            System.out.println("User Command:");
+            String line = in.nextLine();
+            String stringAfterCommand = line.substring(line.indexOf(' ') + 1);
+            String[] userCommandArray = line.split(" ");
+
+            // Check command
+            switch (userCommandArray[0]) {
+            case "bye":
+                exitMartin();
+                break label;
+            case "list":
+                displayListOfTasks();
+                break;
+            case "mark":
+                markTask(userCommandArray);
+                break;
+            case "unmark":
+                unmarkTask(userCommandArray);
+                break;
+            case "todo":
+                addTodoTask(stringAfterCommand);
+                break;
+            case "deadline":
+                addDeadlineTask(stringAfterCommand);
+                break;
+            case "event":
+                addEventTask(stringAfterCommand);
+                break;
+            }
+        }
+    }
 
     private static void storeActivity(Task activity) {
         tasks[taskCounter] = activity;
@@ -15,7 +52,7 @@ public class Martin {
     }
 
     private static void printHorizontalLine() {
-        System.out.println("_".repeat(40));
+        System.out.println("_".repeat(HORIZONTAL_LINE));
     }
 
     private static void printer(String line) {
@@ -24,72 +61,97 @@ public class Martin {
         printHorizontalLine();
     }
 
-    public static void main(String[] args) {
+    private static Scanner getFirstScanner() {
         printer("Martin:\nHello sir my name is Martin.\nWhat can I do for you today?");
 
         Scanner in = new Scanner(System.in);
         String line = "";
+        return in;
+    }
 
-        while (true) {
-            // Ask for next command
-            System.out.println("User Command:");
-            line = in.nextLine();
-            String stringAfterCommand = line.substring(line.indexOf(' ') + 1);
-            String[] userCommandArray = line.split(" ");
+    /**
+     * Display end message and quits
+     */
+    private static void exitMartin() {
+        printer("Martin:\nBye Sir, see you tomorrow!");
+        return;
+    }
 
-            // Check command
-            if (userCommandArray[0].equals("bye")) {
-                // Bye
-                printer("Martin:\nBye Sir, see you tomorrow!");
-                break;
-            }
-            if (userCommandArray[0].equals("list")) {
-                // List Activities
-                printHorizontalLine();
-                System.out.println("Martin's To-do List:");
-                for (int i = 0; i < taskCounter; i++) {
-                    String marked = tasks[i].getTaskDone() ? "X" : " ";
-                    System.out.printf("%d. [%s] %s\n", i + 1, marked, tasks[i].getTask());
-                }
-                printHorizontalLine();
-            } else if (userCommandArray[0].equals("mark")) {
-                // Mark activity as done
-                int itemIndex = Integer.parseInt(userCommandArray[1]);
-                if (itemIndex > tasks.length) {
-                    System.out.println("Martin:\nSir, you have keyed in an invalid task index. Please try again");
-                    continue;
-                }
-                int markIndex = itemIndex - 1;
-                tasks[markIndex].markAsDone();
-                printer(String.format("Martin:\nGood news Sir! I have marked %d. %s as done.", itemIndex, tasks[markIndex].getTask()));
-            } else if (userCommandArray[0].equals("unmark")) {
-                // Unmark activity as done
-                int itemIndex = Integer.parseInt(userCommandArray[1]);
-                if (itemIndex > tasks.length) {
-                    System.out.println("Martin:\nSir, you have keyed in an invalid task index. Please try again");
-                    continue;
-                }
-                int markIndex = itemIndex - 1;
-                tasks[markIndex].unmarkAsDone();
-                printer(String.format("Martin:\nSorry Sir, I have to remark %d. %s as undone.", itemIndex, tasks[markIndex].getTask()));
-            } else if (userCommandArray[0].equals("todo")) {
-                Todo task = new Todo(stringAfterCommand);
-                storeActivity(task);
-                printer("Martin:\nYes Sir, I have added this task to my list:\n" + "    [" + task.getTypeOfTask() + "]" + "[ ] " + task.getTask() + "\nI currently have " + taskCounter + " tasks in the list.");
-            } else if (userCommandArray[0].equals("deadline")) {
-                String description = stringAfterCommand.substring(0,stringAfterCommand.indexOf("/by") - 1);
-                String deadline = stringAfterCommand.substring(stringAfterCommand.indexOf("/by") + 4);
-                Deadline task = new Deadline(description,deadline);
-                storeActivity(task);
-                printer("Martin:\nYes Sir, I have added this task to my list:\n" + "    [" + task.getTypeOfTask() + "]" + "[ ] " + task.getTask() + "\nI currently have " + taskCounter + " tasks in the list.");
-            } else if (userCommandArray[0].equals("event")) {
-                String description = stringAfterCommand.substring(0,stringAfterCommand.indexOf("/from") - 1);
-                String startDate = stringAfterCommand.substring(stringAfterCommand.indexOf("/from") + 6, stringAfterCommand.indexOf("/to") - 1);
-                String endDate = stringAfterCommand.substring(stringAfterCommand.indexOf("/to") + 4);
-                Event task = new Event(description,startDate,endDate);
-                storeActivity(task);
-                printer("Martin:\nYes Sir, I have added this task to my list:\n" + "    [" + task.getTypeOfTask() + "]" + "[ ] " + task.getTask() + "\nI currently have " + taskCounter + " tasks in the list.");
-            }
+    /**
+     * Displays list of tasks from tasks[]
+     */
+    private static void displayListOfTasks() {
+        printHorizontalLine();
+        System.out.println("Martin's To-do List:");
+        for (int i = 0; i < taskCounter; i++) {
+            String marked = tasks[i].getTaskDone() ? "X" : " ";
+            System.out.printf("%d. [%s] %s\n", i + 1, marked, tasks[i].getTask());
         }
+        printHorizontalLine();
+    }
+
+    /**
+     * Mark task as done. Task index is second argument in user input
+     */
+    private static void markTask(String[] userCommandArray) {
+        int itemIndex = Integer.parseInt(userCommandArray[1]);
+        if (isInvalidIndex(itemIndex)) return;
+        int markIndex = itemIndex - 1;
+        tasks[markIndex].markAsDone();
+        printer(String.format("Martin:\nGood news Sir! I have marked %d. %s as done.", itemIndex, tasks[markIndex].getTask()));
+    }
+    /**
+     * Unmark task as done. Task index is second argument in user input
+     */
+    private static void unmarkTask(String[] userCommandArray) {
+        int itemIndex = Integer.parseInt(userCommandArray[1]);
+        if (isInvalidIndex(itemIndex)) return;
+        int markIndex = itemIndex - 1;
+        tasks[markIndex].unmarkAsDone();
+        printer(String.format("Martin:\nSorry Sir, I have to remark %d. %s as undone.", itemIndex, tasks[markIndex].getTask()));
+    }
+
+    /**
+     * Check if user input argument is a valid task index
+     * @return true if invalid, false if valid
+     */
+    private static boolean isInvalidIndex(int itemIndex) {
+        if (itemIndex > tasks.length) {
+            System.out.println("Martin:\nSir, you have keyed in an invalid task index. Please try again");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Add a Todo task to tasks[]
+     */
+    private static void addTodoTask(String stringAfterCommand) {
+        Todo task = new Todo(stringAfterCommand);
+        storeActivity(task);
+        printer("Martin:\nYes Sir, I have added this task to my list:\n" + "    [" + task.getTypeOfTask() + "]" + "[ ] " + task.getTask() + "\nI currently have " + taskCounter + " tasks in the list.");
+    }
+
+    /**
+     * Add a Event task to tasks[]
+     */
+    private static void addEventTask(String stringAfterCommand) {
+        String description = stringAfterCommand.substring(0, stringAfterCommand.indexOf("/from") - 1);
+        String startDate = stringAfterCommand.substring(stringAfterCommand.indexOf("/from") + 6, stringAfterCommand.indexOf("/to") - 1);
+        String endDate = stringAfterCommand.substring(stringAfterCommand.indexOf("/to") + 4);
+        Event task = new Event(description,startDate,endDate);
+        storeActivity(task);
+        printer("Martin:\nYes Sir, I have added this task to my list:\n" + "    [" + task.getTypeOfTask() + "]" + "[ ] " + task.getTask() + "\nI currently have " + taskCounter + " tasks in the list.");
+    }
+
+    /**
+     * Add a Deadline task to tasks[]
+     */
+    private static void addDeadlineTask(String stringAfterCommand) {
+        String description = stringAfterCommand.substring(0, stringAfterCommand.indexOf("/by") - 1);
+        String deadline = stringAfterCommand.substring(stringAfterCommand.indexOf("/by") + 4);
+        Deadline task = new Deadline(description,deadline);
+        storeActivity(task);
+        printer("Martin:\nYes Sir, I have added this task to my list:\n" + "    [" + task.getTypeOfTask() + "]" + "[ ] " + task.getTask() + "\nI currently have " + taskCounter + " tasks in the list.");
     }
 }
